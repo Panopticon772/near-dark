b85576810994076da6a872058c1955afeb565572
Chip <Panopticon772@gmail.com>
2019-01-01 23:41:48 -0500

    Design improvements.

--- app/assets/scripts/functions.js
+++ app/assets/scripts/functions.js
@@ -2,7 +2,19 @@
 const get_main_nav = document.querySelector("#js-menu");
 const nav_toggle = document.querySelector("#js-navbar-toggle");
 
-// when clicking logo drop down should appear
+// get footer button
+const getFooterBtn = document.querySelector("#footer-btn");
+
+// // when clicking logo drop down should appear
 nav_toggle.addEventListener("click", () => {
     get_main_nav.classList.toggle("active");
+});
+
+const back_to_top = () => {
+    document.body.scrollTop = 0; // safari
+    document.documentElement.scrollTop = 0; // chrome, firefox, IE
+}
+
+getFooterBtn.addEventListener("click", () => {
+    back_to_top();
 });
